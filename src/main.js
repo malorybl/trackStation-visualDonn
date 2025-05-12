8cbac18aa8009eff7d5ebc83e0e1fca899d37727
emmacdh <emma.chautems2000@gmail.com>
2025-05-12 11:23:56 +0200

    last animations

--- src/main.js
+++ src/main.js
@@ -329,14 +329,13 @@ gsap.fromTo(".women-rap-artist-content",
 );
 
 
-//a travailler ça ne fonctionne pas
-gsap.fromTo(".section-quote-kendrick",
-  { opacity: 0, y: "10vh" },
+gsap.fromTo(".billboard-explication",
+  { opacity: 0, y: "30vh" },
   {
     scrollTrigger: {
-      trigger: ".section-quote-kendrick",
-      start: "top 95%",
-      end: "top 30%",
+      trigger: ".billboard-explication",
+      start: "top 25%",
+      end: "top 50%",
       scrub: 2,
     },
     opacity: 1,
@@ -345,3 +344,133 @@ gsap.fromTo(".section-quote-kendrick",
   }
 );
 
+
+gsap.fromTo(".section-compare",
+  { opacity: 0, x: "100vw" },
+  {
+    opacity: 1,
+    x: "0vw",
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: ".section-compare",
+      start: "top 40%",
+      end: "top 60%",
+      scrub: 2,
+    }
+  }
+);
+
+
+
+// boom instantané de la section VS Battle
+gsap.fromTo(".section-vs-battle",
+  { opacity: 0, scale: 0.8 },
+  {
+    opacity: 1,
+    scale: 1,
+    ease: "back.out(1.5)",
+    scrollTrigger: {
+      trigger: ".section-vs-battle",
+      start: "top 35%",       // quand le top de la section atteint 75% du viewport
+      toggleActions: "play reverse play reverse", // joue a chaque scroll dessus, sans scrub
+      markers: false
+    }
+  }
+);
+
+
+gsap.fromTo(".section-vs-context",
+  { opacity: 0, y: "30vh" },
+  {
+    scrollTrigger: {
+      trigger: ".section-vs-context",
+      start: "top 25%",
+      end: "top 50%",
+      scrub: 2,
+    },
+    opacity: 1,
+    ease: "power1.inOut",
+    y: "0vh"
+  }
+);
+
+
+gsap.fromTo(
+  ".not-like-us-content",
+  { x: "100%" },                // point de départ (hors écran à droite)
+  {
+    x: "0%",                    // arrive à sa position normale
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: ".section-not-like-us",
+      start: "top bottom",      // quand le haut de la section atteint le bas du viewport
+      end:   "top top",         // jusqu'à ce que ce même haut atteigne le haut du viewport
+      scrub: true
+    }
+  }
+);
+
+
+
+gsap.fromTo(".grammys-content",
+  { y: "100%" },          // part en bas
+  {
+    y: "0%",              // arrive à sa position normale
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".section-grammys",
+      start: "top bottom",  // synchronisé avec l'entrée de la section
+      end:   "top top",     // jusqu'à ce qu'elle soit entièrement en vue
+      scrub: true,
+    }
+  }
+);
+
+
+
+gsap.fromTo(".section-kanye",
+  { opacity: 0, y: "30vh" },
+  {
+    scrollTrigger: {
+      trigger: ".section-kanye",
+      start: "top 25%",
+      end: "top 50%",
+      scrub: 2,
+    },
+    opacity: 1,
+    ease: "power1.inOut",
+    y: "0vh"
+  }
+);
+
+
+gsap.fromTo(
+  ".section-kanye-poster",
+  { x: "-100%" },                // point de départ (hors écran à droite)
+  {
+    x: "0%",                    // arrive à sa position normale
+    ease: "power1.inOut",
+    scrollTrigger: {
+      trigger: ".section-kanye-poster",
+      start: "top bottom",      // quand le haut de la section atteint le bas du viewport
+      end:   "top top",         // jusqu'à ce que ce même haut atteigne le haut du viewport
+      scrub: true
+    }
+  }
+);
+
+
+gsap.fromTo(".section-kanye-quote",
+  { scale: 0.5 },
+  {
+    scrollTrigger: {
+      trigger: ".section-kanye-quote",
+      start: "top 20%",
+      end: "+=700",
+      scrub: true,
+      pin: true
+    },
+    scale: 1.5,
+    ease: "power1.inOut"
+  }
+);
